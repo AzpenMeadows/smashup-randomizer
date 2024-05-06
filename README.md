eeffcb20a28ccc5a245eb0cced180022b687c659
AzpenMeadows <azpenmeadows@outlook.com>
2024-05-06 07:48:54 -0500

    sycining between devices

---
+++ README.md
@@ -0,0 +1,33 @@
+# Smash Up Randomizer
+
+- [Goal](#goal)
+- [Features](#features)
+
+## Goal
+
+Create an app to randomly choose factions for players
+
+## Project Links
+
+- [Live app]()
+
+
+## Features
+
+- Add factions to pool by selecting owned expansions
+- Choose between 2 and 4 players
+- Distribute factions in the order specified in Smash Up rules
+- Option to reroll
+
+## Author
+Created by Jason Konrad
+
+- [GitHub | AzpenMeadows](https://github.com/AzpenMeadows)
+
+## Acknowledgements
+
+**Smash Up was created by Alderic Entertainment Group**  
+
+*Disclaimer: this app was not developed by AEG
+
+[AEG | Smash Up Game](https://www.alderac.com/smash-up/)
---
+++ scripts/scripts.js
@@ -0,0 +1,83 @@
+let factionList = []
+let pool = []
+let activeArray
+
+const expanCore = ["Aliens", "Dinosaurs", "Ninjas", "Pirates", "Robots", "Tricksters", "Wizards", "Zombies"]
+const expanPretty = ["Fairies", "Kitty Cats", "Mythic Horses", "Princesses"]
+
+const expanListEl = document.querySelector("#js-expansions__list-el")
+const expanCoreEl = document.querySelector("#js-expan-core-el")
+const expanPrettyEl = document.querySelector("#js-expan-pretty-el")
+const btnEl = document.querySelector("#js-randomize-btn")
+const playerCt2El =  document.querySelector("#js-player-ct-two")
+const playerCt3El =  document.querySelector("#js-player-ct-three")
+const playerCt4El =  document.querySelector("#js-player-ct-four")
+const player1El = document.querySelector("#js-factions__player1-el")
+const player2El = document.querySelector("#js-factions__player2-el")
+const player3El = document.querySelector("#js-factions__player3-el")
+const player4El = document.querySelector("#js-factions__player4-el")
+
+expanCoreEl.addEventListener("change", function() {
+    activeArray = expanCore
+    if (this.checked) {
+        addExpan()
+    } else {
+        removeExpan()
+    }
+})
+
+expanPrettyEl.addEventListener("change", function() {
+    activeArray = expanPretty
+    if (this.checked) {
+        addExpan()
+    } else {
+        removeExpan()
+    }
+})
+
+btnEl.addEventListener("click", function () {
+    setPool()
+    let htmlPlayer1 = `<li>${getFaction()}</li><li>${getFaction()}</li>`
+    let htmlPlayer2 = `<li>${getFaction()}</li><li>${getFaction()}</li>`
+    let htmlPlayer3 = `<li>${getFaction()}</li><li>${getFaction()}</li>`
+    let htmlPlayer4 = `<li>${getFaction()}</li><li>${getFaction()}</li>`
+    if (playerCt2El.checked) {
+        player1El.innerHTML = htmlPlayer1
+        player2El.innerHTML = htmlPlayer2
+    } else if (playerCt3El.checked) {
+        player1El.innerHTML = htmlPlayer1
+        player2El.innerHTML = htmlPlayer2
+        player3El.innerHTML = htmlPlayer3
+    } else if (playerCt4El.checked) {
+        player1El.innerHTML = htmlPlayer1
+        player2El.innerHTML = htmlPlayer2
+        player3El.innerHTML = htmlPlayer3
+        player4El.innerHTML = htmlPlayer4
+    }
+})
+
+function addExpan() {
+    for(let i = 0; i < activeArray.length; i++) {
+        factionList.push(activeArray[i])
+    }
+}
+
+function removeExpan() {
+    for(let i = 0; i < activeArray.length; i++) {
+        let removeItem = factionList.indexOf(activeArray[i])
+        factionList.splice(removeItem, 1)
+    }
+}
+
+function getFaction() {
+    let i = Math.floor(Math.random() * pool.length)
+    let faction = pool[i]
+    pool.splice([i], 1)
+    return faction
+}
+
+function setPool() {
+    for(let i = 0; i < factionList.length; i++) {
+        pool.push(factionList[i])
+}
+}
